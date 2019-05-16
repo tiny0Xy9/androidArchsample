c83a03811a03b76fe06619e7c914213d0689d744
tiny <tinyLab0Xy9@gmail.com>
2019-05-17 02:47:06 +0900

    初回コミット

---
+++ ArchitectureTest/app/src/main/java/app/pete/com/architecturetest/MainActivity.kt
@@ -0,0 +1,72 @@
+package app.pete.com.architecturetest
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.NavGraph
+import androidx.navigation.NavInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    var isNavigation:Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            Log.d("mainactivity","floatingbutton")
+
+            isNavigation = if(!isNavigation) true  else  false
+            findViewById<FrameLayout>(R.id.rootid).visibility = isVisible(isNavigation)
+
+        }
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun isVisible(isvisible:Boolean):Int {
+        createhostNaV()
+        return if (isvisible)View.GONE else View.VISIBLE
+    }
+
+    fun createhostNaV(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        val navController = navHost!!.navController
+        val navInflater = navController.navInflater
+        val graph = navInflater.inflate(R.navigation.navigation_graph)
+        graph.startDestination = R.id.firstFragment
+        navController.graph = graph
+    }
+}
